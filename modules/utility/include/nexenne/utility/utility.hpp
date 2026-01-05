f34af0c3622395f328afa3df7d18f3ca01e6643d
Nicolò Plebani <nicoloplebani@mailbox.org>
2026-01-05 17:39:20 +0200

    feat(utility): add non_null pointer wrapper

--- modules/utility/include/nexenne/utility/utility.hpp
+++ modules/utility/include/nexenne/utility/utility.hpp
@@ -7,6 +7,7 @@
 
 #include <nexenne/utility/defer.hpp>
 #include <nexenne/utility/narrow_cast.hpp>
+#include <nexenne/utility/non_null.hpp>
 #include <nexenne/utility/overloaded.hpp>
 #include <nexenne/utility/scope_guard.hpp>
 
