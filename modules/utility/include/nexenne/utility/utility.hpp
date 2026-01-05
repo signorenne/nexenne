8b0cd3dc87ea689731b0172e99437508b521231a
Nicolò Plebani <nicoloplebani@mailbox.org>
2026-01-05 18:54:38 +0200

    feat(utility): add defer scope-exit guard

--- modules/utility/include/nexenne/utility/utility.hpp
+++ modules/utility/include/nexenne/utility/utility.hpp
@@ -5,6 +5,7 @@
  * @brief Umbrella header for the nexenne::utility module.
  */
 
+#include <nexenne/utility/defer.hpp>
 #include <nexenne/utility/narrow_cast.hpp>
 #include <nexenne/utility/overloaded.hpp>
 
