fbceceadfc4c70d83fda06f7fe64cd2ca58d39c7
Nicolò Plebani <nicoloplebani@mailbox.org>
2026-01-05 16:45:19 +0200

    feat(utility): add scope_guard with dismiss and engage

--- modules/utility/include/nexenne/utility/utility.hpp
+++ modules/utility/include/nexenne/utility/utility.hpp
@@ -8,5 +8,6 @@
 #include <nexenne/utility/defer.hpp>
 #include <nexenne/utility/narrow_cast.hpp>
 #include <nexenne/utility/overloaded.hpp>
+#include <nexenne/utility/scope_guard.hpp>
 
 namespace nexenne::utility {}
