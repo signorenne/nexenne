68409b41982ed30eea0268a083333535dd08ffc8
Nicolò Plebani <nicoloplebani@mailbox.org>
2026-01-21 20:23:47 +0200

    feat(container): add object_pool

--- modules/container/include/nexenne/container/container.hpp
+++ modules/container/include/nexenne/container/container.hpp
@@ -12,6 +12,7 @@
 #include <nexenne/container/gap_buffer.hpp>
 #include <nexenne/container/heap.hpp>
 #include <nexenne/container/linear_arena.hpp>
+#include <nexenne/container/object_pool.hpp>
 #include <nexenne/container/ring_buffer.hpp>
 #include <nexenne/container/scratch_pad.hpp>
 #include <nexenne/container/small_vector.hpp>
